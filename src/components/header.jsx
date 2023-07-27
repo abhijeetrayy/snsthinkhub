46100b8eb8e15eb4983c85370d610c7573144c83
Abhijeet Ray <abhijeet11ray@gmail.com>
2023-07-27 06:30:47 +0530

    header ui changed (background svg)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -97,7 +97,19 @@ const Header = () => {
     // </div>
 
     <div className="w-full flex flex-col justify-center items-center min-h-[350px]">
-      <div className="w-full flex justify-center items-center">
+      <div
+        className="z-0 absolute inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        ></div>
+      </div>
+      <div className="z-10 w-full flex justify-center items-center">
         <div className="max-w-3xl grow my-3">
           <div className="flex w-full justify-between  mb-10">
             <svg
@@ -146,7 +158,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <span className="text-gray-600 text-xl md:text-4xl font-bold block">
+      <span className="z-20 text-gray-600 text-xl md:text-4xl font-bold block">
         Education for Excellence
       </span>
       <h2 className=" mb-5 font-sans text-2xl leading-5 font-bold tracking-tight text-indigo-500 sm:text-3xl md:text-4xl xl:text-5xl  sm:leading-none object-cover">
