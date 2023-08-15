34b48a9873527b66725753b10957463240303187
Abhijeet Ray <abhijeet11ray@gmail.com>
2023-08-15 13:43:14 +0530

    minor ui update

--- src/components/workedwith.jsx
+++ src/components/workedwith.jsx
@@ -5,17 +5,26 @@ import qis from "../public/photos/colageLogo/qis.jpeg";
 import Image from "next/image";
 export default function workedwith() {
   return (
-    <section className="lg:flex flex-col items-center ">
-      <div className="">
-        <div className="min-w-full h-full flex gap-5 items-center justify-evenly overflow-x-scroll overflow-y-scroll whitespace-nowrap scroll-smooth scrollbar-thin scrollbar-thumb-indigo-500 py-4">
-          <div href="#" className="">
+    <section className="w-full lg:flex flex-col items-center ">
+      <div className="w-full flex justify-center">
+        <div className="w-full h-full flex items-center justify-center gap-10 overflow-x-scroll overflow-y-scroll whitespace-nowrap scroll-smooth scrollbar-thin scrollbar-thumb-indigo-500 py-4">
+          <div href="#" className="relative">
             <Image className="w-54" width={60} src={gtibit} alt="" />
+            <div class="opacity-0 md:hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs text-gray-600 -top-20">
+              Guru Tegh Bahadur Institute of Technology
+            </div>
           </div>
-          <div href="#" className="">
+          <div href="#" className="relative">
             <Image className="w-54" width={70} src={global} alt="" />
+            <div class="opacity-0 md:hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs text-gray-600 -top-20">
+              Global Engineering College
+            </div>
           </div>
-          <div href="#" className="">
+          <div href="#" className="relative">
             <Image className="w-54" width={60} src={qis} alt="" />
+            <div class="opacity-0 md:hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs text-gray-600 -top-20">
+              QIS
+            </div>
           </div>
         </div>
       </div>
