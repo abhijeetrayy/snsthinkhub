58df39786e2a79183443a1632a1d686946ce7088
Abhijeet Ray <abhijeet11ray@gmail.com>
2023-09-01 22:42:21 +0530

    major auth n middleware update - optimized

--- src/app/home/admin/layout.jsx
+++ src/app/home/admin/layout.jsx
@@ -6,7 +6,7 @@ import { signIn, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Layout({ children }) {
   const { data: session, status } = useSession();
-  if (!session) {
+  if (status == "unauthenticated") {
     return (
       <>
         Click here to signin:{" "}
