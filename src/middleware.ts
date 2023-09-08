be0adc6072035f5b42fb015860e7e014f5aa3b4b
Abhijeet Ray <abhijeet11ray@gmail.com>
2023-09-08 07:46:42 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(req: NextRequest) {
 
  
   if(req.nextUrl.pathname == "/"){
-    return NextResponse.rewrite(new URL ("/home", req.url))
+    return NextResponse.redirect(new URL ("/home", req.url))
   }
   
 }
